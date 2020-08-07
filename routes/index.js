966a0b0873121c84884d4e420c1d25c6e8ca18e8
William Richardson <wcrich01@gmail.com>
2020-08-07 00:50:01 -0400

    finished converting html pages to pug. Corrected script.js pathway in layout and testimonials

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home Page' });
+    res.render('index', { title: 'Home Page' });
+    next();
 });
 
 router.get('/testimonials', function(req, res, next) {
-    res.render('testimonials', { title: 'Testimonials' })
+    res.render('testimonials', { title: 'Testimonials' });
+    next();
 });
 
 router.get('/shop', function(req, res, next) {
-    res.render('shop', { title: 'Shop' })
+    res.render('shop', { title: 'Shop' });
+    next();
 });
 
 module.exports = router;
