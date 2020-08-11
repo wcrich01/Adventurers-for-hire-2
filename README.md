dab954c1e3b92d590fbf330dc3e96ce175fb9cee
William Richardson <wcrich01@gmail.com>
2020-08-11 18:14:37 -0400

    got api working to log to the console

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Created a randomize function to add random items to the shop page.
 
 Using Express created a CRUD app to make a shopping cart. In the shopping cart include the ability to delete an item from the list. The item count should display the correct price and set up the total at the bottom.
 
+
+
+
+
---
+++ public/javascripts/shop-api.js
@@ -0,0 +1,111 @@
+//JS for the /shop page API
+
+var arrAllEquipment = [];
+var arr = [];
+var arrDesiredEquipmentUrls = [];
+var arrDesiredEquipment = [
+//    {
+//        name: "Axe",
+//        weapon_range: "Martial Melee",
+//        cost: 5
+//        
+//    },
+];
+
+const baseUrl = "https://www.dnd5eapi.co";
+const weaponCategoryUrl = "/api/equipment-categories/weapon"
+fetch(baseUrl + weaponCategoryUrl)
+    .then(res => res.json())
+    .then(inventoryResult => {
+        //console.log(inventoryResult.equipment);
+        arrAllEquipment = inventoryResult.equipment;
+        while(arr.length < 10){
+            var r = Math.floor(Math.random() * 37);
+            if(arr.indexOf(r) === -1) arr.push(r);
+        }
+        arr.sort();
+        //console.log(arr);
+
+
+        arr.forEach(element => {
+            arrDesiredEquipmentUrls.push(arrAllEquipment[parseInt(element)]);
+        });
+        //console.log(arrDesiredEquipmentUrls);
+        arrDesiredEquipmentUrls.forEach(element => {
+                fetch(baseUrl + element.url)
+                    .then(res => res.json())
+                    .then(equipmentResult => {
+                            arrDesiredEquipment.push(equipmentResult);
+                    });
+                });
+                console.log(arrDesiredEquipment);           
+    });
+    
+
+// Function to display Equipment for sale in store
+
+//Promise.all(arrDesiredEquipment).then(results => {
+//    var equipmentAvail = results.map(data => ({
+//        name: arrDesiredEquipment.name,
+//        type: arrDesiredEquipment.weapon_range,
+//        cost: arrDesiredEquipment.cost
+//    }));
+//    console.log(equipmentAvail);
+//    displayEquipment(equipmentAvail);
+//});
+
+
+const displayEquipment = equipmentAvail => {
+    const equipmentAvailHTMLString = equipmentAvail
+        .map(
+            equip =>
+            `
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">${equip.name}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${equip.weapon_range}</h6>
+                        <p class="card-text">${equip.cost} gp.</p>
+                        <a href="#" class="card-link">Add to Cart</a>
+                    </div>
+                </div>
+            `
+        )
+        .join("");
+    shopInventory.innerHTML = equipmentAvailHTMLString;
+};
+
+
+//function equipmentAvailHTML(arrDesiredEquipment){
+//    var equipment = '';
+//    var availEquipment;
+//    for (let i = 0; i < arrDesiredEquipment.length; i+= 1) {
+//        availEquipment = arrDesiredEquipment[i];
+//        equipment += `.card(style='width: 18rem;')><div class="card-body"><h5 class="card-title">` + arrDesiredEquipment.name + `</h5>`;
+//        equipment+= `<h6 class="card-subtitle mb-2 text-muted">` + arrDesiredEquipment.category_range + `</h6>`;
+//        equipment=+ `<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card content.</p><a href="#" class="card-link">Add to Cart</a></div></div>`;
+//    }
+//    return equipment;
+//}
+//print(equipmentAvailHTML(arrDesiredEquipment));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
